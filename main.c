f394599e76f805da968015853ae1c01ea11575a9
kuromi24 <daliacorralruiz@gmail.com>
2025-03-29 18:21:46 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,32 @@
+# include <stdio.h>
+# include "common.h "
+# include "piece_gen.h "
+# include "defects.h "
+# include "weight_dims.h "
+# include "timing.h "
+# include "classify.h "
+# include "format.h "
+# include "display_log.h "
+# include "stats.h "
+
+int main () {
+    struct Piece piece ;
+    struct Destination dest = {0 , 0 , 0};
+    char buffer [200];
+    for ( int i = 0; i < 5; i ++)
+    {
+        generatePiece (& piece ) ;
+        checkDefects (& piece ) ;
+        measureWeightAndDims (& piece ) ;
+        setStationTimes (& piece ) ;
+        classifyPiece (& piece , & dest ) ;
+        formatData (& piece , buffer , 200) ;
+        displayAndLog ( buffer ) ;
+        updateStats (& piece , & dest ) ;
+    }
+    printStats () ;
+ return 0;
+}
+
+
+
