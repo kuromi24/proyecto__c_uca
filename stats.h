840e7f118270e4a2493213926f5d58892a2ade19
kuromi24 <daliacorralruiz@gmail.com>
2025-03-31 18:51:44 +0200

    stats.h

--- stats.h
+++ stats.h
@@ -2,9 +2,6 @@
 #define STATS_H
 
 float calculateAverage(int values[], int size);
-int totalPieces = 0; // Contador de piezas procesadas en total
-float totalWeight = 0.0; // Acumulador del peso total de las piezas
-int totalTimeStations[NUM_STATIONS] = {0}; // Array para almacenar tiempos acumulados por estación
 void updateStats(struct Piece *piece, struct Destination *dest);
 void printStats();
 
